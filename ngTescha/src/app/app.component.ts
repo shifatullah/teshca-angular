03431eced01684f9d5d879f3dfb7c5b7a43f95c4
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2021-06-09 00:53:32 +1000

    initial ng new angular project and side bar navigation using angular material

---
+++ ngTescha/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav'
+import { BreakpointObserver } from '@angular/cdk/layout'
+import { ViewChild } from '@angular/core'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'ngTescha';
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
+  constructor(private observer: BreakpointObserver) {}
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
+
+}
